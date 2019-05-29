57ab866a589b38b3eab235d5aeb706f9b2ea2c6f
WolfEye <artemdobrovolksyi1@gmail.com>
2019-05-29 16:05:22 +0300

    new project

---
+++ task/resources/views/pages/book.blade.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'task')}}</title>
+    </head>
+    <body>
+
+    </body>
+</html>
